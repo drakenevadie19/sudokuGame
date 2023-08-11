017d5b6e814cb242230fb02ce6c756fde63575a6
drakenevadie19 <drakennievadje19@gmail.com>
2023-08-11 18:19:21 +0700

    New Changes: Using router.navigate to connect Home Page to Main Maze

--- sudokuGame/src/app/components/main-page/main-page.component.ts
+++ sudokuGame/src/app/components/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShareService } from 'src/app/share.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -10,12 +11,20 @@ export class MainPageComponent {
   solve: boolean = false;
   play: boolean = false;
 
-  constructor(private shareService: ShareService) {
+  constructor(private shareService: ShareService, private router: Router) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    this.shareService.solveSignal.subscribe(signal =>this.solve = signal);
+    this.shareService.playMaze.subscribe(signal => this.play = signal);
+
+    if (this.solve == true && this.play == false) {
+      this.router.navigate(['/main/solveMaze']);
+    } else {
+      this.router.navigate(['/main/playMaze']);
+    }
 
   }
 
